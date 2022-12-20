f73d59a7b5371c20f07e80e2cbea1f87167cff80
denpetlevany <denpetlevany@gmail.com>
2022-12-20 19:02:50 +0200

    fix PropTypes

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -20,6 +20,10 @@ export default function FeedbackOptions({ options, onLeaveFeedback }) {
 };
 
 FeedbackOptions.propTypes = {
-    options: PropTypes.object.isRequired,
+    options: PropTypes.shape({
+        good: PropTypes.number.isRequired,
+        neutral: PropTypes.number.isRequired,
+        bad: PropTypes.number.isRequired,
+  }),
     onLeaveFeedback: PropTypes.func.isRequired,
 };
